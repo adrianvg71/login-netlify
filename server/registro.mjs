85e8b6860bcf1c0f6c6866d989f135edaffe93ad
adrianvg71 <adrianvg71@educastur.es>
2023-10-26 21:58:56 +0200

    añadido handler a el servidor express

--- server/registro.mjs
+++ server/registro.mjs
@@ -1,33 +1,20 @@
-import express from 'express';
-import fs from 'fs';
-import cors from 'cors';
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
 
-const port = process.env.PORT || 3000;
 const app = express();
+const router = express.Router();
 
-app.use(express.json());
-
-app.use(express.static('../client/public'));
-//app.use('/data', express.static('data'));
-
-// Habilita CORS para permitir solicitudes desde cualquier origen
+// Configura CORS para permitir solicitudes desde cualquier origen
 app.use(cors());
 
-// Reemplaza 'dist' con la carpeta correcta en la que se generan los archivos de construcción.
-app.use(express.static('dist'));
-
-// Configura Express para servir index.html como la página principal
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
-
 // Ruta para el registro de usuarios
-app.post('/registro', (req, res) => {
+router.post('/', (req, res) => {
   const { correo, nombre, contraseña } = req.body;
 
   try {
     // Leer el archivo JSON existente
-    const data = fs.readFileSync('../client/public/data/users.json', 'utf8');
+    const data = fs.readFileSync('./public/data/users.json', 'utf8');
     const users = JSON.parse(data);
 
     // Verificar si el correo ya existe en la matriz de usuarios
@@ -39,7 +26,7 @@ app.post('/registro', (req, res) => {
       users.users.push({ correo, nombre, contraseña });
 
       // Guardar la matriz actualizada en el archivo JSON (usa la misma ruta)
-      fs.writeFileSync('../client/public/data/users.json', JSON.stringify(users, null, 2), 'utf8');
+      fs.writeFileSync('./public/data/users.json', JSON.stringify(users, null, 2), 'utf8');
 
       res.status(200).json({ message: 'Usuario registrado con éxito' });
     }
@@ -49,6 +36,6 @@ app.post('/registro', (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Servidor en ejecución en http://localhost:${port}`);
-});
+module.exports = {
+  handler: app
+};
